a587cee44e5d00ff1682f2c345136624cdf45f13
Ap1kkk <apikmaster2@yandex.ru>
2024-09-23 12:14:45 +0300

    Init

---
+++ backend/src/main/java/ru/ngtu/sabacc/SabaccApplication.java
@@ -0,0 +1,13 @@
+package ru.ngtu.sabacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabaccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SabaccApplication.class, args);
+    }
+
+}
