88e72118bfc9aedafca0ed3739f2dd1f9c878569
Ap1kkk <apikmaster2@yandex.ru>
2024-10-02 20:00:23 +0300

    Expanded configuration

--- backend/src/main/java/ru/ngtu/sabacc/SabaccApplication.java
+++ backend/src/main/java/ru/ngtu/sabacc/SabaccApplication.java
@@ -2,8 +2,6 @@ package ru.ngtu.sabacc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import ru.ngtu.sabacc.config.user.UserConfigProperties;
 
 @SpringBootApplication
 public class SabaccApplication {
---
+++ backend/src/main/java/ru/ngtu/sabacc/config/websocket/WebsocketConfigProperties.java
@@ -0,0 +1,23 @@
+package ru.ngtu.sabacc.config.websocket;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties("websocket")
+public class WebsocketConfigProperties {
+    private Stomp stomp;
+    private Broker broker;
+
+    @Data
+    public static class Stomp {
+        private String endpoint;
+        private String allowedOrigin;
+    }
+
+    @Data
+    public static class Broker {
+        private String[] destinationPrefixes;
+        private String[] applicationDestinationPrefixes;
+    }
+}
